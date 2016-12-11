038bd97c6f96546b63e51e938ad5065a7c544023
qfeys <qfeys@skynet.be>
2016-12-11 18:46:13 +0100

    UI shenanigans

--- Assets/Scripts/Rendering/DataDisplay.cs
+++ Assets/Scripts/Rendering/DataDisplay.cs
@@ -10,10 +10,13 @@ namespace Assets.Scripts.Rendering
 {
     class DataDisplay : MonoBehaviour
     {
+
+        public GameObject standardInfoPanel;
+
         public void Update()
         {
             List<Sensor> sens = God.TheOne.Sensors;
-
+            ////////####################################### Change this so it calls the correct elements!
             GameObject overviewWindow = transform.GetChild(1).GetChild(0).gameObject;
             var EthylInfo = overviewWindow.transform.GetChild(1).GetChild(1).GetComponent<InfoTable>();
             EthylInfo.SetInfo(sens[1].Data());
