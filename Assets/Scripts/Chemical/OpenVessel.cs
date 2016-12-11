88d75ed220733f8bbaaa7de549d7c7c66077e1f4
qfeys <qfeys@skynet.be>
2016-12-11 08:36:30 +0100

    Add OpenVessel

---
+++ Assets/Scripts/Chemical/OpenVessel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Chemical
+{
+    class OpenVessel : Vessel
+    {
+        float mass;
+        float Volume;
+        Mixture mixture;
+
+
+        public float Pressure { get { return 1; } }
+
+        public void AddMixture(Mixture mix, float mass)
+        {
+            if (mixture == null)
+                mixture = mix;
+            else
+                mixture.Add(mix, mass / this.mass);
+            this.mass += mass;
+        }
+
+        public Mixture RemoveMixture(float volume, out float mass)
+        {
+            mass = volume * mixture.Density;
+            this.mass -= mass;
+            return mixture;
+        }
+
+        public void Update(float dT)
+        {
+            if (mixture != null)
+            {
+                RecalculateVolume();
+                mixture.React(dT);
+            }
+        }
+
+        private void RecalculateVolume()
+        {
+            mixture.pressure = 1;
+            if(mixture.Density * mass > Volume)
+            {
+                throw new Exception("Open vessel has overflown");
+            }
+        }
+    }
+}
