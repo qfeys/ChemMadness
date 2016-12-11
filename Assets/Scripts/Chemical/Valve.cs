48794406b6c72d7b02d0e5732cd7c11b986cce07
qfeys <qfeys@skynet.be>
2016-12-11 09:32:40 +0100

    Add Valve

--- Assets/Scripts/Chemical/Valve.cs
+++ Assets/Scripts/Chemical/Valve.cs
@@ -5,22 +5,18 @@ using System.Text;
 
 namespace Assets.Scripts.Chemical
 {
-    class Pump : Pipe, Controlled
+    class Valve : Pipe, Controlled
     {
+        public float Control { get; set; }
 
-        float pumpPressure;
-
-        public Pump(Vessel entrance, Vessel exit, float length, float diameter, float dP) 
+        public Valve(Vessel entrance, Vessel exit, float length, float diameter) 
             : base(entrance, exit, length, diameter)
         {
-            pumpPressure = dP;
         }
-
-        public float Control { get; set; }
 
         public override void Update(float dT)
         {
-            float Dp = (entrance.Pressure - exit.Pressure) * 10000 + pumpPressure * Control; // in Pa
+            float Dp = (entrance.Pressure - exit.Pressure) * 10000; // in Pa
             float rho = mixture.Density;
             float dim = diameter * Control;
             if (diameter * diameter / 32 < 2 * dT)    // fast system, inmediatly go to steady state
@@ -31,8 +27,7 @@ namespace Assets.Scripts.Chemical
             {
                 flowSpeed += (Dp / rho / length - 32 * flowSpeed / dim / dim) * dT;
             }
-            if (flowSpeed > 500) flowSpeed = 500;   // To stay sain
-            float internalMass = rho * area * length;
+            if (flowSpeed > 500) flowSpeed = 500;
             float movedMass = rho * area * flowSpeed * dT;
             float movedVolume = area * flowSpeed * dT;
             if (flowSpeed > 0)
@@ -40,14 +35,14 @@ namespace Assets.Scripts.Chemical
                 exit.AddMixture(mixture, movedMass);
                 float mass;
                 Mixture newMix = entrance.RemoveMixture(movedVolume, out mass);
-                mixture.Add(newMix, mass / (internalMass - movedMass));
+                mixture.Add(newMix, mass / (InternalMass - movedMass));
             }
             else if (flowSpeed < 0)
             {
                 entrance.AddMixture(mixture, movedMass);
                 float mass;
                 Mixture newMix = exit.RemoveMixture(movedVolume, out mass);
-                mixture.Add(newMix, mass / (internalMass - movedMass));
+                mixture.Add(newMix, mass / (InternalMass - movedMass));
             }
         }
     }
